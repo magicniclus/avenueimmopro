14f3f9c213a4d0dba984b58eee5dc6d447fe48df
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-01 16:01:35 +0200

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import setDrawerOpen from "./drawerSlice";
 // import adminSlice from "./adminSlice";
 // import userReducer from "./createUserSlice";
 
 export const store = configureStore({
   reducer: {
+    drawer: setDrawerOpen,
     // createUser: userReducer,
     // admin: adminSlice,
   },
