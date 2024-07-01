bd8f83f339956a75623bb25975fa6a5331f12ac0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-01 10:49:06 +0200

    Update Header.tsx

--- src/components/tailwindUi/header/Header.tsx
+++ src/components/tailwindUi/header/Header.tsx
@@ -56,11 +56,7 @@ export default function Header({
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt=""
-            />
+            <img className="h-8 w-auto" src="/logo.png" alt="" />
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -165,11 +161,7 @@ export default function Header({
           <div className="flex items-center justify-between">
             <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
+              <img className="h-8 w-auto" src="/logo.png" alt="" />
             </a>
             <button
               type="button"
