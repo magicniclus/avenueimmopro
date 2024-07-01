141243151db150f0f9e2310b7d288bd98715c414
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-01 19:40:35 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <StoreProvider>
       <html lang="fr">
         <head>
-          <link rel="icon" href="/favicon.png" sizes="any" />
+          <link rel="icon" href="/favicon-bg.png" sizes="any" />
         </head>
         <body className={inter.className}>{children}</body>
         <GoogleTagManager gtmId={GTM} />
