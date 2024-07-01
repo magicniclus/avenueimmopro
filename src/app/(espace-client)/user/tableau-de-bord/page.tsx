af9769c1408fa06035d8eaac80d7f2a9af5f63de
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-01 16:54:56 +0200

    Update page.tsx

--- src/app/(espace-client)/user/tableau-de-bord/page.tsx
+++ src/app/(espace-client)/user/tableau-de-bord/page.tsx
@@ -2,6 +2,7 @@
 
 import Drawer from "@/components/tailwindUi/drawers/Drawer";
 import SideBarLayout from "@/components/tailwindUi/layout/SideBarLayout";
+import List from "@/components/tailwindUi/list/List";
 import { setDrawerOpen } from "@/redux/drawerSlice";
 import { useDispatch } from "react-redux";
 
@@ -17,13 +18,9 @@ const Page = () => {
       <Drawer />
       <SideBarLayout>
         <h1 className="text-gray-700 font-bold text-2xl">Tableau de bord</h1>
-        <button
-          type="button"
-          onClick={() => setOpen(true)}
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md"
-        >
-          Ouvrir le tiroir
-        </button>
+        <div className="mt-14">
+          <List />
+        </div>
       </SideBarLayout>
     </div>
   );
