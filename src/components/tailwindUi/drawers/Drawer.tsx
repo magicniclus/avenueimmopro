127e4e6d49951dedd362e63a9a57870cf4d31fcd
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-01 16:01:29 +0200

    Create Drawer.tsx

---
+++ src/components/tailwindUi/drawers/Drawer.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { setDrawerOpen } from "@/redux/drawerSlice";
+import { RootState } from "@/redux/store";
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { useDispatch, useSelector } from "react-redux";
+
+const Drawer = () => {
+  const dispatch = useDispatch();
+  const open = useSelector((state: RootState) => state.drawer.drawerOpen);
+
+  const setOpen = (value: boolean) => {
+    dispatch(setDrawerOpen(value));
+  };
+
+  return (
+    <Dialog
+      className="relative z-100"
+      style={{ zIndex: "1000" }}
+      open={open}
+      onClose={setOpen}
+    >
+      <div className="fixed inset-0" />
+      <div className="fixed inset-0 overflow-hidden">
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+            <DialogPanel className="pointer-events-auto w-screen max-w-2xl transform transition duration-500 ease-in-out data-[closed]:translate-x-full sm:duration-700">
+              <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                <div className="px-4 sm:px-6">
+                  <div className="flex items-start justify-between">
+                    <DialogTitle className="text-base font-semibold leading-6 text-gray-900 sr-only">
+                      Panel title
+                    </DialogTitle>
+                    <div className="ml-3 flex h-7 items-center">
+                      <button
+                        type="button"
+                        className="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        onClick={() => setOpen(false)}
+                      >
+                        <span className="absolute -inset-2.5" />
+                        <span className="sr-only">Close panel</span>
+                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                  {/* Your content */}
+                </div>
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  );
+};
+
+export default Drawer;
