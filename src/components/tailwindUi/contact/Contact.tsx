2deec9813dc636377b4487bb8a6f517514dda88d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-02 17:33:46 +0200

    Update Contact.tsx

--- src/components/tailwindUi/contact/Contact.tsx
+++ src/components/tailwindUi/contact/Contact.tsx
@@ -1,7 +1,7 @@
 import {
-  BugAntIcon,
   ChatBubbleLeftRightIcon,
   ComputerDesktopIcon,
+  PhoneIcon,
 } from "@heroicons/react/24/outline";
 
 const Contact = () => {
@@ -9,15 +9,37 @@ const Contact = () => {
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="mx-auto max-w-2xl sm:text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Contact sales
+          Contact
         </h2>
         <p className="mt-2 text-lg leading-8 text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
+          Contactez notre équipe pour toute question ou demande de support.
         </p>
       </div>
       <div className="mx-auto mt-20 max-w-lg space-y-16">
         <div className="flex gap-x-6">
-          <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-indigo-600">
+          <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-blue-600">
+            <PhoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
+          </div>
+          <div>
+            <h3 className="text-base font-semibold leading-7 text-gray-900">
+              Support client téléphonique
+            </h3>
+            <p className="mt-2 leading-7 text-gray-600">
+              Contactez le support pour toute question sur nos produits ou
+              services.
+            </p>
+            <p className="mt-4">
+              <a
+                href="tel:0968523252"
+                className="text-sm font-semibold leading-6 text-blue-600"
+              >
+                09 68 52 32 52
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="flex gap-x-6">
+          <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-blue-600">
             <ChatBubbleLeftRightIcon
               className="h-6 w-6 text-white"
               aria-hidden="true"
@@ -25,46 +47,24 @@ const Contact = () => {
           </div>
           <div>
             <h3 className="text-base font-semibold leading-7 text-gray-900">
-              Sales support
+              Support après-vente
             </h3>
             <p className="mt-2 leading-7 text-gray-600">
-              Ut cursus est ut amet. Lobortis eget egestas leo vitae eget
-              porttitor risus blandit. Nunc a in lorem vel iaculis porttitor.
+              Contactez le support pour toute question sur nos produits ou
+              services.
             </p>
             <p className="mt-4">
               <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-indigo-600"
+                href="mailto:support@avenue-immo.fr"
+                className="text-sm font-semibold leading-6 text-blue-600"
               >
-                Contact us <span aria-hidden="true">&rarr;</span>
+                Contact <span aria-hidden="true">&rarr;</span>
               </a>
             </p>
           </div>
         </div>
         <div className="flex gap-x-6">
-          <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-indigo-600">
-            <BugAntIcon className="h-6 w-6 text-white" aria-hidden="true" />
-          </div>
-          <div>
-            <h3 className="text-base font-semibold leading-7 text-gray-900">
-              Bug reports
-            </h3>
-            <p className="mt-2 leading-7 text-gray-600">
-              Expedita qui non ut quia ipsum voluptatum ipsam pariatur. Culpa
-              vitae ipsum minus eius vero quo quibusdam.
-            </p>
-            <p className="mt-4">
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-indigo-600"
-              >
-                Report a bug <span aria-hidden="true">&rarr;</span>
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="flex gap-x-6">
-          <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-indigo-600">
+          <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-blue-600">
             <ComputerDesktopIcon
               className="h-6 w-6 text-white"
               aria-hidden="true"
@@ -72,18 +72,18 @@ const Contact = () => {
           </div>
           <div>
             <h3 className="text-base font-semibold leading-7 text-gray-900">
-              Technical support
+              Support technique
             </h3>
             <p className="mt-2 leading-7 text-gray-600">
-              Sint aut modi porro consequatur architecto commodi qui
-              consequatur. Dignissimos adipisci minima.
+              Signalez un bug ou un problème technique sur notre site web.
             </p>
             <p className="mt-4">
               <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-indigo-600"
+                href="mailto:technique@avenue-immo.fr"
+                className="text-sm font-semibold leading-6 text-blue-600"
               >
-                Join our Discord <span aria-hidden="true">&rarr;</span>
+                Reporter un probleme technique{" "}
+                <span aria-hidden="true">&rarr;</span>
               </a>
             </p>
           </div>
