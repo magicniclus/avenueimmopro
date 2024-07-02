70f2ab69f0caa54813f1a6493a52975fa3e7bb06
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-02 10:48:42 +0200

    Update Footer.tsx

--- src/components/tailwindUi/footer/Footer.tsx
+++ src/components/tailwindUi/footer/Footer.tsx
@@ -197,7 +197,7 @@ export default function Footer({
         </div>
         <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
           <p className="text-xs leading-5 text-gray-400">
-            &copy; 2020 Your Company, Inc. All rights reserved.
+            &copy; 2020 avenue-immo.fr, Inc. All rights reserved.
           </p>
         </div>
       </div>
