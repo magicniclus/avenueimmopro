e095a568df102abc2e4b0e7ce627e79ecde47ac9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-02 11:36:55 +0200

    Create page.tsx

---
+++ src/app/espace-pro/performances/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Drawer from "@/components/tailwindUi/drawers/Drawer";
+import SideBarLayout from "@/components/tailwindUi/layout/SideBarLayout";
+
+const page = () => {
+  return (
+    <div className="relative">
+      <Drawer />
+      <SideBarLayout>
+        <h1 className="text-gray-700 font-bold text-2xl">Perfomances</h1>
+      </SideBarLayout>
+    </div>
+  );
+};
+
+export default page;
