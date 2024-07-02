c0b2adc0d015e82d202fd3e05b990a36753ed6d9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-02 10:48:40 +0200

    Update page.tsx

--- src/app/espace-pro/tableau-de-bord/page.tsx
+++ src/app/espace-pro/tableau-de-bord/page.tsx
@@ -1,10 +1,38 @@
+"use client";
+
+import ListLink from "@/components/perso/ListLink";
+import Drawer from "@/components/tailwindUi/drawers/Drawer";
+import FAQ from "@/components/tailwindUi/faq/FAQ";
 import SideBarLayout from "@/components/tailwindUi/layout/SideBarLayout";
+import List from "@/components/tailwindUi/list/List";
+import Stats from "@/components/tailwindUi/stats/Stats";
+import { setDrawerOpen } from "@/redux/drawerSlice";
+import { useDispatch } from "react-redux";
 
 const Page = () => {
+  const dispatch = useDispatch();
+
+  const setOpen = (value: boolean) => {
+    dispatch(setDrawerOpen(value));
+  };
+
   return (
-    <SideBarLayout>
-      <h1>Tableau de bord</h1>
-    </SideBarLayout>
+    <div className="relative">
+      <Drawer />
+      <SideBarLayout>
+        <h1 className="text-gray-700 font-bold text-2xl">Tableau de bord</h1>
+        <div className="mt-14">
+          <Stats />
+        </div>
+        <div className="mt-14">
+          <List />
+        </div>
+        <div className="flex justify-between flex-col md:flex-row mt-14">
+          <ListLink />
+          <FAQ />
+        </div>
+      </SideBarLayout>
+    </div>
   );
 };
 
