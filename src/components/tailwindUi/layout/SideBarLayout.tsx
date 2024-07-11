745bbbba11e2537271128c76a6c104a8b34d9087
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-11 10:47:42 +0200

    Update SideBarLayout.tsx

--- src/components/tailwindUi/layout/SideBarLayout.tsx
+++ src/components/tailwindUi/layout/SideBarLayout.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 
 "use client";
 
-import { getUserInfo, isUserLoggedIn, logOut } from "@/firebase/auth"; // Importez la fonction logOut depuis son chemin
+import { getUserInfo, isUserLoggedIn, logOut } from "@/firebase/auth";
+import { getDataByRoute } from "@/firebase/database";
 import {
   Dialog,
   DialogPanel,
@@ -23,7 +25,7 @@ import {
   UserIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 function classNames(...classes: string[]) {
@@ -33,56 +35,67 @@ function classNames(...classes: string[]) {
 const SideBarLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const pathName = usePathname();
+  const searchParams = useSearchParams();
+  let userId = searchParams.get("id");
 
   const [navigation, setNavigation] = useState([
     {
       name: "Tableau de bord",
-      href: "/espace-pro/tableau-de-bord",
+      href: `/espace-pro/tableau-de-bord`,
       icon: HomeIcon,
       current: false,
     },
     {
       name: "Leads",
-      href: "/espace-pro/leads",
+      href: `/espace-pro/leads`,
       icon: UserIcon,
       current: false,
     },
     {
       name: "Performances",
-      href: "/espace-pro/performances",
+      href: `/espace-pro/performances`,
       icon: ArrowTrendingUpIcon,
       current: false,
     },
     {
       name: "Contact",
-      href: "/espace-pro/contact",
+      href: `/espace-pro/contact`,
       icon: EnvelopeIcon,
       current: false,
     },
   ]);
 
-  const teams = [
-    { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
-    // { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-    // { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
-  ];
+  const updateUrlsWithId = (id: string) => {
+    setNavigation((prevNavigation) =>
+      prevNavigation.map((item) => ({
+        ...item,
+        href: `${item.href.split("?")[0]}?id=${id}`,
+      }))
+    );
+  };
+
+  const updateCurrentNavigation = (currentPath: string) => {
+    setNavigation((prevNavigation) =>
+      prevNavigation.map((item) => ({
+        ...item,
+        current: item.href.split("?")[0] === currentPath,
+      }))
+    );
+  };
+
   const userNavigation = [
     { name: "Your profile", href: "#" },
     { name: "Sign out", href: "#", onClick: () => handleSignOut() },
   ];
 
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [isLoaded, setIsLoaded] = useState(true);
-  const [letters, setLetters] = useState<string>(""); // Provide a default value for letters
+  const [isLoading, setIsLoading] = useState(true);
+  const [letters, setLetters] = useState<string>("");
+  const [idIsOk, setIdIsOk] = useState(false);
 
   useEffect(() => {
-    const currentPath = pathName;
-    setNavigation((prevNavigation) =>
-      prevNavigation.map((item) => ({
-        ...item,
-        current: item.href === currentPath,
-      }))
-    );
+    const currentPath = pathName.split("?")[0];
+    updateCurrentNavigation(currentPath);
   }, [pathName]);
 
   const handleSignOut = async () => {
@@ -95,34 +108,57 @@ const SideBarLayout = ({ children }: { children: React.ReactNode }) => {
   };
 
   useEffect(() => {
-    setIsLoaded(true);
     const checkUserLoggedIn = async () => {
       const loggedIn = await isUserLoggedIn();
       if (!loggedIn) {
-        await setIsLoaded(false);
+        setIsLoading(false);
         router.push("/espace-pro/connexion");
+        return;
       }
-      setIsLoaded(false);
     };
-
     checkUserLoggedIn();
-  }, [router]);
-
-  useEffect(() => {
-    getUserInfo()
-      .then((user) => {
-        if (user && user.email) {
-          setLetters(user.email);
-        } else {
-          setLetters("");
+    if (!idIsOk) {
+      getUserInfo().then((data) => {
+        if (data) {
+          userId = data.uid;
+          if (!searchParams.get("id")) {
+            router.replace(`${pathName}?id=${userId}`);
+            updateUrlsWithId(userId);
+          }
+          getDataByRoute(`agents/${userId}`).then((user) => {
+            if (user && user.informations.Abonnement.actif) {
+              setIsLoading(false);
+              const currentPath = pathName.split("?")[0];
+              updateCurrentNavigation(currentPath);
+            } else {
+              router.push(`/espace-pro/abonnement?id=${userId}`);
+            }
+          });
         }
-      })
-      .catch((error) => {
-        console.log(error);
+        setIdIsOk(true);
       });
+    }
   }, []);
 
-  if (isLoaded) {
+  useEffect(() => {
+    if (!idIsOk) {
+      getUserInfo()
+        .then((user) => {
+          if (user && user.email) {
+            setLetters(user.email);
+          } else {
+            setLetters("");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      setIdIsOk(true);
+    }
+  }, []);
+
+  if (isLoading) {
     return (
       <div className="flex justify-center items-center h-screen w-screen bg-white">
         <div className="text-center">
@@ -184,7 +220,6 @@ const SideBarLayout = ({ children }: { children: React.ReactNode }) => {
                       </button>
                     </div>
                   </TransitionChild>
-                  {/* Sidebar component, swap this element with another sidebar if you like */}
                   <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10">
                     <div className="flex h-16 shrink-0 items-center">
                       <img
@@ -218,36 +253,11 @@ const SideBarLayout = ({ children }: { children: React.ReactNode }) => {
                             ))}
                           </ul>
                         </li>
-                        {/* <li>
-                          <div className="text-xs font-semibold leading-6 text-gray-400">
-                            Your teams
-                          </div>
-                          <ul role="list" className="-mx-2 mt-2 space-y-1">
-                            {teams.map((team) => (
-                              <li key={team.name}>
-                                <a
-                                  href={team.href}
-                                  className={classNames(
-                                    team.current
-                                      ? "bg-gray-800 text-white"
-                                      : "text-gray-400 hover:bg-gray-800 hover:text-white",
-                                    "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
-                                  )}
-                                >
-                                  <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                                    {team.initial}
-                                  </span>
-                                  <span className="truncate">{team.name}</span>
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </li> */}
                         <li className="mt-auto">
                           <a
                             href="#"
                             className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
-                            onClick={handleSignOut} // Appeler handleSignOut au clic
+                            onClick={handleSignOut}
                           >
                             <Cog6ToothIcon
                               className="h-6 w-6 shrink-0"
@@ -265,9 +275,7 @@ const SideBarLayout = ({ children }: { children: React.ReactNode }) => {
           </Dialog>
         </Transition>
 
-        {/* Static sidebar for desktop */}
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-          {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
             <div className="flex h-16 shrink-0 items-center">
               <img
@@ -301,36 +309,11 @@ const SideBarLayout = ({ children }: { children: React.ReactNode }) => {
                     ))}
                   </ul>
                 </li>
-                {/* <li>
-                  <div className="text-xs font-semibold leading-6 text-gray-400">
-                    Your teams
-                  </div>
-                  <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
-                        <a
-                          href={team.href}
-                          className={classNames(
-                            team.current
-                              ? "bg-gray-800 text-white"
-                              : "text-gray-400 hover:bg-gray-800 hover:text-white",
-                            "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
-                          )}
-                        >
-                          <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                            {team.initial}
-                          </span>
-                          <span className="truncate">{team.name}</span>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li> */}
                 <li className="mt-auto">
                   <a
                     href="#"
                     className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
-                    onClick={handleSignOut} // Appeler handleSignOut au clic
+                    onClick={handleSignOut}
                   >
                     <Cog6ToothIcon
                       className="h-6 w-6 shrink-0"
@@ -355,89 +338,50 @@ const SideBarLayout = ({ children }: { children: React.ReactNode }) => {
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </button>
 
-            {/* Separator */}
-            <div
-              className="h-6 w-px bg-gray-900/10 lg:hidden"
-              aria-hidden="true"
-            />
-
             <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6 justify-end">
-              {/* <form className="relative flex flex-1" action="#" method="GET">
-                <label htmlFor="search-field" className="sr-only">
-                  Search
-                </label>
-                <MagnifyingGlassIcon
-                  className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                <input
-                  id="search-field"
-                  className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-                  placeholder="Search..."
-                  type="search"
-                  name="search"
-                />
-              </form> */}
-              <div className="flex items-center gap-x-4 lg:gap-x-6">
-                {/* <button
-                  type="button"
-                  className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+              <Menu as="div" className="relative flex items-center">
+                <MenuButton className="-m-1.5 flex items-center p-1.5">
+                  <span className="sr-only">Open user menu</span>
+                  <div className="text-gray-600">{letters}</div>
+                  <span className="hidden lg:flex lg:items-center">
+                    <ChevronDownIcon
+                      className="ml-2 h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </MenuButton>
+                <Transition
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
                 >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button> */}
-
-                {/* Separator */}
-                {/* <div
-                  className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
-                  aria-hidden="true"
-                /> */}
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative">
-                  <MenuButton className="-m-1.5 flex items-center p-1.5">
-                    <span className="sr-only">Open user menu</span>
-                    <div className="text-gray-600">{letters}</div>
-                    <span className="hidden lg:flex lg:items-center">
-                      <ChevronDownIcon
-                        className="ml-2 h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </span>
-                  </MenuButton>
-                  <Transition
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <MenuItems className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                      {userNavigation.map((item) => (
-                        <MenuItem key={item.name}>
-                          {({ active }) => (
-                            <a
-                              href={item.href}
-                              onClick={
-                                item.name === "Sign out"
-                                  ? handleSignOut
-                                  : undefined
-                              }
-                              className={classNames(
-                                active ? "bg-gray-50" : "",
-                                "block px-3 py-1 text-sm leading-6 text-gray-900"
-                              )}
-                            >
-                              {item.name}
-                            </a>
-                          )}
-                        </MenuItem>
-                      ))}
-                    </MenuItems>
-                  </Transition>
-                </Menu>
-              </div>
+                  <MenuItems className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                    {userNavigation.map((item) => (
+                      <MenuItem key={item.name}>
+                        {({ active }) => (
+                          <a
+                            href={item.href}
+                            onClick={
+                              item.name === "Sign out"
+                                ? handleSignOut
+                                : undefined
+                            }
+                            className={classNames(
+                              active ? "bg-gray-50" : "",
+                              "block px-3 py-1 text-sm leading-6 text-gray-900"
+                            )}
+                          >
+                            {item.name}
+                          </a>
+                        )}
+                      </MenuItem>
+                    ))}
+                  </MenuItems>
+                </Transition>
+              </Menu>
             </div>
           </div>
 
